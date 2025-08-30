fc18d582c4cad3c21e5c809a2974a0fcd2598428
Orizu <michael@limpiar.online>
2025-08-30 23:25:37 +0100

    new

--- src/app/services/nexcessService.js
+++ src/app/services/nexcessService.js
@@ -16,6 +16,10 @@ const transporter = nodemailer.createTransport({
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
+
+    tls: {
+      rejectUnauthorized: false, // helps avoid self-signed cert issues
+    },
 });
 
 // OTP Generator
